4a8dd916f65173b79e09303d16ba0f16bd497525
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-01 12:49:12 -0300

    Update scrollup.js

--- scrollup.js
+++ scrollup.js
@@ -1,1 +1,20 @@
+//Con JQuery
+$(document).ready(function() {
+	
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 100) {
+					$('.scrollup').fadeIn();
+				} else {
+					$('.scrollup').fadeOut();
+				}
+			});
 
+			$('.scrollup').on("click", function() {
+				$("html, body").animate({
+					scrollTop: 0
+				}, 500);
+				return false;
+			});
+
+
+});
